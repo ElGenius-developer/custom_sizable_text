368a5cd5e617a50a84595e4c648ad67389954226
Ahmed Ashraf <ahmed.abdalmola555@gmail.com>
2022-04-18 14:31:04 +0200

    d

--- lib/custom_text.dart
+++ lib/custom_text.dart
@@ -1,7 +1,7 @@
 library custom_text;
 
-import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
 
 class CustomText extends StatelessWidget {
   final String text;
